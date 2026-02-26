96e55f80ee73e5305845f94f298b2c2e19c28288
sssonghg <elly0959@gmail.com>
2026-02-26 17:53:49 +0900

    Initial Commit

---
+++ DGCAN/DGCANApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGCANApp.swift
+//  DGCAN
+//
+//  Created by 송하경 on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct DGCANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
