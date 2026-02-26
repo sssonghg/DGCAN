fbfd456cdffe8e1b8c28c601fd81d2b11bc624a3
sssonghg <elly0959@gmail.com>
2026-02-26 17:56:56 +0900

    Initial Commit

--- DGCAN/ContentView.swift
+++ DGCAN/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+
+            Text("안녕!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
         }
-        .padding()
     }
 }
 
